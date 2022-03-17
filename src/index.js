479582344f9ea7f3e51270a71a6a756c2e733d47
Kwong Aiden <aidenkwong5@gmail.com>
2022-03-18 07:21:10 +0800

    adjust console log

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ const pool = new Pool({
   },
 });
 
-const CREATE_TABLE = `
-CREATE TABLE IF NOT EXISTS cron_test_log (
-  "id" SERIAL PRIMARY KEY,
-  "createdAt" TIMESTAMP NOT NULL
-)
-`;
+// const CREATE_TABLE = `
+// CREATE TABLE IF NOT EXISTS cron_test_log (
+//   "id" SERIAL PRIMARY KEY,
+//   "createdAt" TIMESTAMP NOT NULL
+// )
+// `;
 
 const ADD_LOG = `
 INSERT INTO cron_test_log (createdAt) VALUES ($1)
@@ -26,13 +26,13 @@ const SELECT_LOGS = `
 SELECT * FROM cron_test_log
 `;
 
-pool.query(CREATE_TABLE);
-
-pool.query(ADD_LOG, [new Date().toUTCString()]);
-
-pool
-  .query(SELECT_LOGS)
-  .then((res) => {
-    return console.log(res.rows);
-  })
-  .then(() => exit());
+// pool.query(CREATE_TABLE);
+pool.query(ADD_LOG, [new Date().toUTCString()]).then((res) => {
+  console.log(res.command);
+  pool
+    .query("SELECT count(*) FROM cron_test_log")
+    .then((res) => {
+      return console.log(res.rows[0].count);
+    })
+    .then(() => exit());
+});
